b7f6ac3b28015e59ae27737fb308137fe06c524b
Loscate <simon.perrin@etu.univ-nantes.fr>
2020-04-21 00:56:24 +0200

    parallax header

---
+++ components/HeaderParallax.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-parallax dark height="300" src="parallax_background.png">
+    <v-row justify="space-around" align="center">
+      <div>
+        <h1 class="display-1">Discord patterns</h1>
+        <p>FOSS Templates, on your demand !</p>
+      </div>
+      <div>
+        <v-btn href="https://discord.gg/MFMXamv" target="_blank" outlined="true"
+          >Join our Discord Server !</v-btn
+        >
+      </div>
+    </v-row>
+  </v-parallax>
+</template>
+
+<script>
+export default {}
+</script>
