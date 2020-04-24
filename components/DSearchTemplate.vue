3a7305066197e80256072a8f9a979308b2547c3b
mxb <mxb@mxb.com>
2020-04-24 17:22:51 +0200

    Chnaged a little bit layout on search text field

--- components/DSearchTemplate.vue
+++ components/DSearchTemplate.vue
@@ -1,31 +1,28 @@
 <template>
-  <div>
-    <h2>Awesome template list</h2>
-    <v-row>
-      <v-col sm="4" class="py-0 my-0">
-        <v-text-field
-          rounded
-          outlined
-          :label="$t('listing.SEARCH')"
-          append-icon="mdi-magnify"
-          single-line
-          clearable
-          clear-icon="mdi-close"
-          hide-details
-          autofocus
-          dense
-        ></v-text-field>
-      </v-col>
-      <v-col cols="12" sm="12" class="py-0 my-0">
-        <v-chip-group column multiple :model="selectedTag">
-          <v-chip v-for="(tag, index) in tags" :key="index" outlined filter>
-            <v-icon left>{{ tag.icon }}</v-icon>
-            {{ tag.content }}
-          </v-chip>
-        </v-chip-group>
-      </v-col>
-    </v-row>
-  </div>
+  <v-row>
+    <v-col cols="12" md="6" class="py-0 my-0">
+      <v-text-field
+        rounded
+        outlined
+        :label="$t('listing.SEARCH')"
+        append-icon="mdi-magnify"
+        single-line
+        clearable
+        clear-icon="mdi-close"
+        hide-details
+        autofocus
+        dense
+      ></v-text-field>
+    </v-col>
+    <v-col cols="12" class="py-0 my-0">
+      <v-chip-group column multiple :model="selectedTag">
+        <v-chip v-for="(tag, index) in tags" :key="index" outlined filter>
+          <v-icon left>{{ tag.icon }}</v-icon>
+          {{ tag.content }}
+        </v-chip>
+      </v-chip-group>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
