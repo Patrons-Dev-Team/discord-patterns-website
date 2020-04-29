3299efb623cf852debf948ff995a83fb8c6a014d
mxb <mxb@mxb.com>
2020-04-29 14:56:02 +0200

    New dense layout

---
+++ layouts/dense.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app dark>
+    <navbar />
+    <v-content>
+      <parallax-header class="pa-7" height="auto" />
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <d-footer class="mt-10" />
+  </v-app>
+</template>
+
+<script>
+import Navbar from '~/components/core/Navbar.vue'
+import DFooter from '~/components/core/DFooter.vue'
+import ParallaxHeader from '~/components/HeaderParallax.vue'
+
+export default {
+  components: {
+    Navbar,
+    DFooter,
+    ParallaxHeader
+  }
+}
+</script>
