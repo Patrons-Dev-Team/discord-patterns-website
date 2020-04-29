0bc5130934092a3ba97f7a1496a6384fd2f70d2e
mxb <mxb@mxb.com>
2020-04-29 14:55:45 +0200

    Removed useless data

--- layouts/default.vue
+++ layouts/default.vue
@@ -21,29 +21,6 @@ export default {
     Navbar,
     DFooter,
     ParallaxHeader
-  },
-  data() {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
-    }
   }
 }
 </script>
