e11b2bbbeb496dd73c0c9bb7ece7b47e23d8d6b0
mxb <mxb@mxb.com>
2020-05-19 10:37:10 +0200

    added base url to env

--- nuxt.config.js
+++ nuxt.config.js
@@ -63,7 +63,7 @@ export default async () => {
      * Customize the base url
      */
     router: {
-      base: '/discord-patterns-website/'
+      base: process.env.BASE_URL || '/discord-patterns-website/'
     },
     /*
      ** Global CSS
